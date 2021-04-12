7111e9378ab1059bd5370ea2c7a28d5862a16519
mdunn17 <mdunn17@my.wctc.edu>
2021-04-12 13:21:36 -0500

    Option 1 - Display Categories

--- Program.cs
+++ Program.cs
@@ -14,7 +14,6 @@ namespace NorthwindConsole
         {
             logger.Info("Program started");
 
-            Console.WriteLine("Hello World!");
             try
             {
                 string choice;
@@ -25,6 +24,22 @@ namespace NorthwindConsole
                     Console.WriteLine("\"q\" to quit");
                     choice = Console.ReadLine();
                     Console.Clear();
+                    logger.Info($"Option {choice} selected");
+                    if (choice == "1")
+                    {
+                        var db = new NWConsole_96_medContext();
+                        var query = db.Categories.OrderBy(p => p.CategoryName);
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"{query.Count()} records returned");
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        foreach (var item in query)
+                        {
+                            Console.WriteLine($"{item.CategoryName} - {item.Description}");
+                        }
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    Console.WriteLine();
 
                 } while (choice.ToLower() != "q");
             }
@@ -32,7 +47,6 @@ namespace NorthwindConsole
             {
                 logger.Error(ex.Message);
             }
-
             logger.Info("Program ended");
         }
     }
