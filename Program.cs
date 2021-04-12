543a3a4bac16f87f0f1b00b37f782264932b25c1
mdunn17 <mdunn17@my.wctc.edu>
2021-04-12 13:17:43 -0500

    program menu

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using NLog.Web;
 using System.IO;
+using System.Linq;
+using NorthwindConsole.Model;
 
 namespace NorthwindConsole
 {
@@ -13,6 +15,23 @@ namespace NorthwindConsole
             logger.Info("Program started");
 
             Console.WriteLine("Hello World!");
+            try
+            {
+                string choice;
+                do
+                {
+                    Console.WriteLine("1) Display Categories");
+                    Console.WriteLine("2) Add Category");
+                    Console.WriteLine("\"q\" to quit");
+                    choice = Console.ReadLine();
+                    Console.Clear();
+
+                } while (choice.ToLower() != "q");
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
 
             logger.Info("Program ended");
         }
